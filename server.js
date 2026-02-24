f9d1f77c82fa7290ddf912e2b7e6a31c28f7e8b6
joshuaaletile-byte <joshuaaletile@gmail.com>
2026-02-24 01:50:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 
+app.use(express.json());
 app.use(express.static(__dirname));
 
-const PORT = process.env.PORT || 3000;
+app.post("/saveUser", (req,res)=>{
+    const user = req.body;
 
-app.listen(PORT, () => console.log("Running"));
+    const data = JSON.parse(fs.readFileSync("users.json"));
+    data[user.name] = user;
+
+    fs.writeFileSync("users.json", JSON.stringify(data,null,2));
+
+    res.send({status:"saved"});
+});
+
+app.get("/getUser/:name",(req,res)=>{
+    const data = JSON.parse(fs.readFileSync("users.json"));
+    res.send(data[req.params.name] || {});
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, ()=>console.log("PH03NIX-GRID Running"));
